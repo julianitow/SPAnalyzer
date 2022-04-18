844fe08d711d98c470477250580039362919eb5a
julien-guillan <guillan.julien@live.com>
2022-04-18 04:06:06 +0200

    TODO: save credentials to EEPROM, next WIFI Setup

--- SPAnalyzer.ino
+++ SPAnalyzer.ino
@@ -3,6 +3,10 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
+#include <string>
+
+#include <EEPROM.h>
+
 #define bleServerName "SOULPOT_ESP32_00"
 #define SERVICE_UUID "80b7f088-0084-43e1-a687-8457bcb2dbc8"
 
@@ -13,7 +17,6 @@ BLECharacteristic wifiParamChar("96c44fd5-c309-4553-a11e-b8457810b94c", BLEChara
 BLEDescriptor wifiParamCharDesc("544bd464-8388-42aa-99cd-81cf6e6042d7");
 
 bool deviceConnected = false;
-int battery = 0;
 
 //Setup callbacks onConnect and onDisconnect
 class MyServerCallbacks: public BLEServerCallbacks {
@@ -32,13 +35,36 @@ class MyCharacteristicCallbacks: public BLECharacteristicCallbacks {
     Serial.println("onRead triggered");
   }
   void onWrite(BLECharacteristic* pCharacteristic) {
-    Serial.println("onWrite triggered");
+    Serial.print("received: ");
     Serial.println(pCharacteristic->getValue().c_str());
+    std::string value = pCharacteristic->getValue();
+    int scIndex = 0;
+    std::string ssid = "";
+    std::string password = "";
+
+    for(int i = 0; i < value.length(); i++) {
+      if(value[i] == ',') {
+        scIndex = i;
+      }
+    }
+
+    for(int i = 0; i < scIndex; i++) {
+      ssid += value[i];
+    }
+    
+    for(int i = scIndex + 1; i < value.length(); i++) {
+      password += value[i];
+    }
+
+    Serial.print("Parsed: ");
+    Serial.print(ssid.c_str());
+    Serial.print(", ");
+    Serial.println(password.c_str());
   }
 };
 
-void setup() {
-  Serial.begin(115200);
+void bluetoothSetup() {
+  Serial.println("Configuration mode detected");
   BLEDevice::init(bleServerName);
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new MyServerCallbacks());
@@ -53,6 +79,11 @@ void setup() {
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pServer->getAdvertising()->start();
   Serial.println("Waiting a client connection to notify...");
+}
+
+void setup() {
+  Serial.begin(115200);
+  bluetoothSetup();
 }
 
 void loop() {
